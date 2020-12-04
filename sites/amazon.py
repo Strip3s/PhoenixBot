202bd2a113d4ad0d243021b33d20f85c1c0d45f3
Strip3s <Jamesabraha@gmail.com>
2020-12-04 16:23:59 -0500

    refactor amazon.py

--- sites/amazon.py
+++ sites/amazon.py
@@ -1,28 +1,22 @@
-import json
-import secrets
-import time
-import os
 import math
-import re
+import time
+import settings
 from datetime import datetime
-from price_parser import parse_price
 
 from amazoncaptcha import AmazonCaptcha
 from chromedriver_py import binary_path  # this will get you the path variable
 from furl import furl
+from price_parser import parse_price
 from selenium import webdriver
 from selenium.common.exceptions import (
-    NoSuchElementException,
-    SessionNotCreatedException,
-    TimeoutException,
+    NoSuchElementException
 )
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-from utils import selenium_utils
-from utils.json_utils import InvalidAutoBuyConfigException
 from utils import create_msg
-from utils.selenium_utils import options, enable_headless, wait_for_element
+from utils import selenium_utils
+from utils.selenium_utils import options, enable_headless
 
 # https://github.com/Hari-Nagarajan/nvidia-bot/blob/master/stores/amazon.py
 
@@ -31,9 +25,7 @@ AMAZON_URLS = {
     "CART_URL": "https://{domain}/gp/aws/cart/add.html",
     "OFFER_URL": "https://{domain}/gp/offer-listing/",
 }
-CHECKOUT_URL = "https://{domain}/gp/cart/desktop/go-to-checkout.html/ref=ox_sc_proceed?partialCheckoutCart=1&isToBeGiftWrappedBefore=0&proceedToRetailCheckout=Proceed+to+checkout&proceedToCheckout=1&cartInitiateId={cart_id}"
-
-AUTOBUY_CONFIG_PATH = "amazon_config.json"
+AMAZON_CHECKOUT_URL = "https://{domain}/gp/cart/desktop/go-to-checkout.html/ref=ox_sc_proceed?partialCheckoutCart=1&isToBeGiftWrappedBefore=0&proceedToRetailCheckout=Proceed+to+checkout&proceedToCheckout=1&cartInitiateId={cart_id}"
 
 SIGN_IN_TEXT = [
     "Hello, Sign in",
@@ -67,7 +59,7 @@ HOME_PAGE_TITLES = [
     "Amazon.it: elettronica, libri, musica, fashion, videogiochi, DVD e tanto altro",
     "Amazon.nl: Groot aanbod, kleine prijzen in o.a. Elektronica, boeken, sport en meer",
 ]
-SHOPING_CART_TITLES = [
+SHOPPING_CART_TITLES = [
     "Amazon.com Shopping Cart",
     "Amazon.ca Shopping Cart",
     "Amazon.co.uk Shopping Basket",
@@ -125,71 +117,42 @@ SHIPPING_ONLY_IF = "FREE Shipping on orders over"
 
 
 class Amazon:
-    def __init__(
-        self,
-        notification_handler,
-        headless=False,
-        checkshipping=False,
-        detailed=False,
-        used=False,
-    ):
-        self.notification_handler = notification_handler
-        self.asin_list = []
-        self.reserve = []
+    def __init__(self, status_signal, image_signal, product, profile, monitor_delay, error_delay, max_price, headless=False, checkshipping=False, detailed=False, used=False):
+        self.status_signal, self.image_signal, self.product, self.profile, self.monitor_delay, self.error_delay, self.max_price = status_signal, image_signal, product, profile, float(monitor_delay), float(error_delay), max_price
+
+        # Amazon SKU Id
+        self.asin = "B0792FQTMJ"  # self.product
         self.checkshipping = checkshipping
         self.detailed = detailed
         self.used = used
-        if os.path.exists(AUTOBUY_CONFIG_PATH):
-            with open(AUTOBUY_CONFIG_PATH) as json_file:
-                try:
-                    config = json.load(json_file)
-                    self.username = config["username"]
-                    self.password = config["password"]
-                    self.asin_groups = int(config["asin_groups"])
-                    self.amazon_website = config.get(
-                        "amazon_website", "smile.amazon.com"
-                    )
-                    for x in range(self.asin_groups):
-                        self.asin_list.append(config[f"asin_list_{x + 1}"])
-                        self.reserve.append(float(config[f"reserve_{x + 1}"]))
-                    # assert isinstance(self.asin_list, list)
-                except Exception:
-                    self.status_signal.emit(create_msg(
-                        "amazon_config.json file not formatted properly: https://github.com/Hari-Nagarajan/nvidia-bot/wiki/Usage#json-configuration",
-                        "error")
-                    )
-                    exit(0)
-        else:
-            self.status_signal.emit(create_msg(
-                "No config file found, see here on how to fix this: https://github.com/Hari-Nagarajan/nvidia-bot/wiki/Usage#json-configuration",
-                "error")
-            )
-            exit(0)
+        self.username = settings.amazon_user
+        self.password = settings.amazon_pass
+
+        starting_msg = "Starting Amazon Task"
+        if settings.dont_buy:
+            starting_msg = "Starting Amazon Task in dev mode - Check Settings page to disable Dev Mode"
+        self.status_signal.emit(create_msg(starting_msg, "normal"))
 
         if headless:
             enable_headless()
-
-        # profile_amz = ".profile-amz"
-        # # keep profile bloat in check
-        # if os.path.isdir(profile_amz):
-        #     os.remove(profile_amz)
         options.add_argument(f"user-data-dir=.profile-amz")
+
         try:
             self.driver = webdriver.Chrome(executable_path=binary_path, options=options)
             self.wait = WebDriverWait(self.driver, 10)
         except Exception as e:
-            self.status_signal.emit(create_msg(f"{e}","error"))
-            exit(1)
+            self.status_signal.emit(create_msg(f"{e}", "error"))
+            # exit(1)
 
         for key in AMAZON_URLS.keys():
-            AMAZON_URLS[key] = AMAZON_URLS[key].format(domain=self.amazon_website)
+            AMAZON_URLS[key] = AMAZON_URLS[key].format(domain="smile.amazon.com") # TODO: Autoconfigure based on Country of the Shipping Address
         self.driver.get(AMAZON_URLS["BASE_URL"])
         self.status_signal.emit(create_msg("Waiting for home page.", "normal"))
         self.check_if_captcha(self.wait_for_pages, HOME_PAGE_TITLES)
         if self.is_logged_in():
             self.status_signal.emit(create_msg("Already logged in", "normal"))
         else:
-            self.status_signal.emit(create_msg("Lets log in.", "normal"))
+            self.status_signal.emit(create_msg("Logging In...", "normal"))
 
             is_smile = "smile" in AMAZON_URLS["BASE_URL"]
             xpath = (
@@ -206,6 +169,20 @@ class Amazon:
                 15
             )  # We can remove this once I get more info on the phone verification page.
 
+        self.status_signal.emit(create_msg("Checking stock for items.", "normal"))
+        checkout_success = False
+        while not checkout_success:
+            checkout_success = self.check_stock()
+            if checkout_success:
+                self.status_signal.emit(create_msg(f"attempting to buy {self.asin}", "normal"))
+                if self.checkout(test=False):
+                    self.status_signal.emit(create_msg(f"bought {self.asin}", "normal"))
+                    break
+                else:
+                    self.status_signal.emit(create_msg(f"checkout for {self.asin} failed", "normal"))
+                    checkout_success = False
+            time.sleep(3)
+
     def is_logged_in(self):
         try:
             text = self.driver.find_element_by_id("nav-link-accountList").text
@@ -214,7 +191,6 @@ class Amazon:
             return False
 
     def login(self):
-
         try:
             self.status_signal.emit(create_msg("Email", "normal"))
             self.driver.find_element_by_xpath('//*[@id="ap_email"]').send_keys(
@@ -225,9 +201,9 @@ class Amazon:
             pass
 
         if self.driver.find_elements_by_xpath('//*[@id="auth-error-message-box"]'):
-            self.status_signal.emit(create_msg("Login failed, check your username in amazon_config.json", "error"))
+            self.status_signal.emit(create_msg("Login failed, check your username within settings page", "error"))
             time.sleep(240)
-            exit(1)
+            # exit(1)
 
         self.status_signal.emit(create_msg("Remember me checkbox", "normal"))
         selenium_utils.button_click_using_xpath(self.driver, '//*[@name="rememberMe"]')
@@ -239,48 +215,19 @@ class Amazon:
 
         self.status_signal.emit(create_msg(f"Logged in as {self.username}", "normal"))
 
-    def run_item(self, delay=3, test=False):
-        self.save_screenshot("start-up")
-        self.status_signal.emit(create_msg("Checking stock for items.", "normal"))
-        checkout_success = False
-        while not checkout_success:
-            pop_list = []
-            for i in range(len(self.asin_list)):
-                for asin in self.asin_list[i]:
-                    checkout_success = self.check_stock(asin, self.reserve[i])
-                    if checkout_success:
-                        self.status_signal.emit(create_msg(f"attempting to buy {asin}", "normal"))
-                        if self.checkout(test=test):
-                            self.status_signal.emit(create_msg(f"bought {asin}", "normal"))
-                            pop_list.append(asin)
-                            break
-                        else:
-                            self.status_signal.emit(create_msg(f"checkout for {asin} failed", "normal"))
-                            checkout_success = False
-                    time.sleep(delay)
-            if pop_list:
-                for asin in pop_list:
-                    for i in range(len(self.asin_list)):
-                        if asin in self.asin_list[i]:
-                            self.asin_list.pop(i)
-                            self.reserve.pop(i)
-                            break
-            if self.asin_list:  # keep bot going if additional ASINs left
-                checkout_success = False
-
-    def check_stock(self, asin, reserve):
+    def check_stock(self):
         if self.checkshipping:
             if self.used:
-                f = furl(AMAZON_URLS["OFFER_URL"] + asin)
+                f = furl(AMAZON_URLS["OFFER_URL"] + self.asin)
             else:
-                f = furl(AMAZON_URLS["OFFER_URL"] + asin + "/ref=olp_f_new&f_new=true")
+                f = furl(AMAZON_URLS["OFFER_URL"] + self.asin + "/ref=olp_f_new&f_new=true")
         else:
             if self.used:
-                f = furl(AMAZON_URLS["OFFER_URL"] + asin + "/f_freeShipping=on")
+                f = furl(AMAZON_URLS["OFFER_URL"] + self.asin + "/f_freeShipping=on")
             else:
                 f = furl(
                     AMAZON_URLS["OFFER_URL"]
-                    + asin
+                    + self.asin
                     + "/ref=olp_f_new&f_new=true&f_freeShipping=on"
                 )
         try:
@@ -311,8 +258,8 @@ class Amazon:
             if ship_float is None or not self.checkshipping:
                 ship_float = 0
 
-            if (ship_float + price_float) <= reserve or math.isclose(
-                (price_float + ship_float), reserve, abs_tol=0.01
+            if (ship_float + price_float) <= self.max_price or math.isclose(
+                (price_float + ship_float), self.max_price, abs_tol=0.01
             ):
                 self.status_signal.emit(create_msg("Item in stock and under reserve!", "normal"))
                 elements[i].click()
@@ -320,20 +267,20 @@ class Amazon:
                 return True
         return False
 
-    def save_screenshot(self, page):
-        file_name = get_timestamp_filename("screenshot-" + page, ".png")
-
-        if self.driver.save_screenshot(file_name):
-            try:
-                self.notification_handler.send_notification(page, file_name)
-            except TimeoutException:
-                self.status_signal.emit(create_msg("Timed out taking screenshot, trying to continue anyway", "normal"))
-                pass
-            except Exception as e:
-                self.status_signal.emit(create_msg(f"Trying to recover from error: {e}", "error"))
-                pass
-        else:
-            self.status_signal.emit(create_msg("Error taking screenshot due to File I/O error", "error"))
+    # def save_screenshot(self, page):
+    #     file_name = get_timestamp_filename("screenshot-" + page, ".png")
+    #
+    #     if self.driver.save_screenshot(file_name):
+    #         try:
+    #             self.notification_handler.send_notification(page, file_name)
+    #         except TimeoutException:
+    #             self.status_signal.emit(create_msg("Timed out taking screenshot, trying to continue anyway", "normal"))
+    #             pass
+    #         except Exception as e:
+    #             self.status_signal.emit(create_msg(f"Trying to recover from error: {e}", "error"))
+    #             pass
+    #     else:
+    #         self.status_signal.emit(create_msg("Error taking screenshot due to File I/O error", "error"))
 
     def save_page_source(self, page):
         """Saves DOM at the current state when called.  This includes state changes from DOM manipulation via JS"""
@@ -358,7 +305,7 @@ class Amazon:
                 time.sleep(5)
                 self.get_captcha_help()
             else:
-                self.save_screenshot("captcha")
+                # self.save_screenshot("captcha")
                 self.driver.find_element_by_xpath(
                     '//*[@id="captchacharacters"]'
                 ).send_keys(solution + Keys.RETURN)
@@ -395,7 +342,7 @@ class Amazon:
                     f"browser is on. There may be a file saved at: amazon-{func.__name__}.png",
                     "error")
                 )
-                self.save_screenshot("title-fail")
+                # self.save_screenshot("title-fail")
                 time.sleep(60)
                 # self.driver.close()
                 self.status_signal.emit(create_msg(f"{e}", "normal"))
@@ -449,7 +396,7 @@ class Amazon:
             else:
                 self.status_signal.emit(create_msg("Couldn't find button after 3 retries. Open a GH issue for this.", "normal"))
                 self.save_page_source("finalize-order-button-fail")
-                self.save_screenshot("finalize-order-button-fail")
+                # self.save_screenshot("finalize-order-button-fail")
         return returnVal
 
     def wait_for_order_completed(self, test):
@@ -458,7 +405,7 @@ class Amazon:
                 self.check_if_captcha(self.wait_for_pages, ORDER_COMPLETE_TITLES)
             except:
                 self.status_signal.emit(create_msg("error during order completion", "error"))
-                self.save_screenshot("order-failed")
+                # self.save_screenshot("order-failed")
                 return False
         else:
             self.status_signal.emit(create_msg("This is a test, so we don't need to wait for the order completed page.", "normal"))
@@ -466,18 +413,18 @@ class Amazon:
 
     def checkout(self, test):
         self.status_signal.emit(create_msg("Waiting for Cart Page", "normal"))
-        self.notification_handler.send_notification("Attempting to checkout")
-        self.check_if_captcha(self.wait_for_pages, SHOPING_CART_TITLES)
-        if self.detailed:
-            self.save_screenshot("waiting-for-cart")
+        self.status_signal.emit(create_msg("Attempting to checkout", "normal"))
+        self.check_if_captcha(self.wait_for_pages, SHOPPING_CART_TITLES)
+        # if self.detailed:
+            # self.save_screenshot("waiting-for-cart")
 
         try:  # This is fast.
             self.status_signal.emit(create_msg("Quick redirect to checkout page", "normal"))
             cart_initiate_id = self.driver.find_element_by_name("cartInitiateId")
             cart_initiate_id = cart_initiate_id.get_attribute("value")
             self.driver.get(
-                CHECKOUT_URL.format(
-                    domain=self.amazon_website, cart_id=cart_initiate_id
+                AMAZON_CHECKOUT_URL.format(
+                    domain="smile.amazon.com", cart_id=cart_initiate_id
                 )
             )
         except:
@@ -487,7 +434,7 @@ class Amazon:
                     '//*[@id="hlb-ptc-btn-native"]'
                 ).click()
             except:
-                self.save_screenshot("start-checkout-fail")
+                # self.save_screenshot("start-checkout-fail")
                 self.status_signal.emit(create_msg("Failed to checkout. Returning to stock check.", "normal"))
                 return False
 
@@ -495,13 +442,13 @@ class Amazon:
         self.wait_for_pyo_page()
 
         self.status_signal.emit(create_msg("Attempting to Finish checkout", "normal"))
-        if self.detailed:
-            self.save_screenshot("finish-checkout")
+        # if self.detailed:
+            # self.save_screenshot("finish-checkout")
 
         if not self.finalize_order_button(test):
             self.status_signal.emit(create_msg("Failed to click finalize the order", "normal"))
-            if self.detailed:
-                self.save_screenshot("finalize-fail")
+            # if self.detailed:
+            # self.save_screenshot("finalize-fail")
             return False
 
         self.status_signal.emit(create_msg("Waiting for Order completed page.", "normal"))
@@ -510,7 +457,7 @@ class Amazon:
             return False
 
         self.status_signal.emit(create_msg("Order Placed.", "normal"))
-        self.save_screenshot("order-placed")
+        #self.save_screenshot("order-placed")
         return True
 
 
