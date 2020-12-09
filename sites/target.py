98be1c5c7dcec1f9cd3133346393385b1ca69d4f
ima9rd <ima9rd@gmail.com>
2020-12-09 17:04:26 -0600

    Significant rewrite to make it more reliable

--- sites/target.py
+++ sites/target.py
@@ -7,33 +7,41 @@ from chromedriver_py import binary_path as driver_path
 from utils import random_delay, send_webhook, create_msg
 from utils.selenium_utils import change_driver
 import settings, time
+from platform import system
+from selenium.webdriver.common.keys import Keys
 
 class Target:
     def __init__(self, task_id, status_signal, image_signal, product, profile, proxy, monitor_delay, error_delay):
         self.task_id, self.status_signal, self.image_signal, self.product, self.profile, self.monitor_delay, self.error_delay = task_id, status_signal, image_signal, product, profile, float(
             monitor_delay), float(error_delay)
-
+        self.xpath_sequence = [
+            {'type': 'method', 'path': '//button[@data-test="orderPickupButton"] | //button[@data-test="shipItButton"]', 'method': self.find_and_click_atc, 'message': 'Added to cart', 'message_type': 'normal'}
+            , {'type': 'button', 'path': '//button[@data-test="espModalContent-declineCoverageButton"]', 'message': 'Declining Coverage', 'message_type': 'normal'}
+            , {'type': 'button', 'path': '//button[@data-test="addToCartModalViewCartCheckout"]', 'message': 'Viewing Cart before Checkout', 'message_type': 'normal'}
+            , {'type': 'button', 'path': '//button[@data-test="checkout-button"]', 'message': 'Checking out', 'message_type': 'normal'}
+            , {'type': 'method', 'path': '//button[@data-test="placeOrderButton"]', 'method': self.submit_order, 'message': 'Submitting order', 'message_type': 'normal'}
+        ]
+        self.possible_interruptions = [
+            {'type': 'input', 'path': '//input[@id="password"]', 'args': {'value': settings.target_pass, 'confirm_button': '//button[@id="login"]'}, 'message': 'Entering password', 'message_type': 'normal'}
+            , {'type': 'input', 'path': '//input[@id="creditCardInput-cardNumber"]', 'args': {'value': self.profile['card_number'], 'confirm_button': '//button[@data-test= "verify-card-button"]'}, 'message': 'Entering CC #', 'message_type': 'normal'}
+            , {'type': 'input', 'path': '//input[@id="creditCardInput-cvv"]', 'args': {'value': self.profile['card_cvv']}, 'message': 'Entering CC #', 'message_type': 'normal'}
+        ]
         starting_msg = "Starting Target"
         self.browser = self.init_driver()
         self.product_image = None
+        self.TIMEOUT_SHORT = 5
         self.TIMEOUT_LONG = 20
-
+        self.did_submit = False
+        self.failed = False
+        self.retry_attempts = 10
         if settings.dont_buy:
             starting_msg = "Starting Target in dev mode; will not actually checkout"
-
         self.status_signal.emit(create_msg(starting_msg, "normal"))
         self.status_signal.emit(create_msg("Logging In..", "normal"))
         self.login()
-        self.monitor()
-        self.atc()
-        self.checkout()
-        self.submit_billing()
-
-        if not settings.dont_buy:
-            self.submit_order()
-            send_webhook("OP", "Target", self.profile["profile_name"], self.task_id, self.product_image)
-        else:
-            self.status_signal.emit(create_msg("Mock Order Placed", "success"))
+        self.img_found = False
+        self.product_loop()
+        send_webhook("OP", "Target", self.profile["profile_name"], self.task_id, self.product_image)
 
     def init_driver(self):
         driver_manager = ChromeDriverManager()
@@ -64,123 +72,128 @@ class Target:
         # Gives it time for the login to complete
         time.sleep(random_delay(self.monitor_delay, settings.random_delay_start, settings.random_delay_stop))
 
-    def monitor(self):
-        img_found = False
-        in_stock = False
+    def product_loop(self):
+        while not self.did_submit and not self.failed:
+            self.monitor()
+            self.atc_and_checkout()
 
+    def check_stock(self, new_tab=False):
+        stock = False
+        if new_tab:
+            windows_before = self.browser.window_handles
+            self.browser.execute_script(f'window.open("{self.product}")')
+            wait(self.browser, 10).until(EC.number_of_windows_to_be(2))
+            new_window = [x for x in self.browser.window_handles if x not in windows_before][0]
+            self.browser.switch_to_window(new_window)
+        if len(self.browser.find_elements_by_xpath('//button[@data-test= "orderPickupButton"]')) > 0 or len(self.browser.find_elements_by_xpath('//button[@data-test= "shipItButton"]')) > 0:
+            stock = True
+        if new_tab:
+            self.browser.close()
+            wait(self.browser, 10).until(EC.number_of_windows_to_be(1))
+            old_window = self.browser.window_handles[0]
+            self.browser.switch_to_window(old_window)
+            return False
+        return stock
+
+    def monitor(self):
+        self.in_stock = False
         self.browser.get(self.product)
         wait(self.browser, self.TIMEOUT_LONG).until(lambda _: self.browser.current_url == self.product)
 
-        while not img_found:
+        while not self.img_found:
             try:
-                if not img_found:
+                if not self.img_found:
                     product_img = self.browser.find_elements_by_class_name('slideDeckPicture')[0].find_element_by_tag_name(
                         "img")
                     self.image_signal.emit(product_img.get_attribute("src"))
                     self.product_image = product_img.get_attribute("src")
-                    img_found = True
+                    self.img_found = True
             except Exception as e:
                 continue
 
-        while not in_stock:
-            add_to_cart_btn = None
-            if len(self.browser.find_elements_by_xpath('//button[@data-test= "orderPickupButton"]')) > 0:
-                add_to_cart_btn = self.browser.find_element_by_xpath('//button[@data-test= "orderPickupButton"]')
-            elif len(self.browser.find_elements_by_xpath('//button[@data-test= "shipItButton"]')) > 0:
-                add_to_cart_btn = self.browser.find_element_by_xpath('//button[@data-test= "shipItButton"]')
+        while not self.in_stock:
+            self.in_stock = self.check_stock()
+            if self.in_stock:
+                continue
             else:
                 self.status_signal.emit(create_msg("Waiting on Restock", "normal"))
                 time.sleep(random_delay(self.monitor_delay, settings.random_delay_start, settings.random_delay_stop))
                 self.browser.refresh()
-                continue
-            self.browser.execute_script("return arguments[0].scrollIntoView(true);", add_to_cart_btn)
-            add_to_cart_btn.click()
-            in_stock = True
-            self.status_signal.emit(create_msg("Added to cart", "normal"))
 
-    def atc(self):
-        declined_ins = False
-        at_checkout = False
-
-        self.status_signal.emit(create_msg("Declining Insurance", "normal"))
-
-        while not declined_ins:
-            try:
-                decline_ins_btn = self.browser.find_element_by_xpath('//button[@data-test= "espModalContent-declineCoverageButton"]')
-                decline_ins_btn.click()
-                declined_ins = True
-            except:
-                continue
-
-        self.status_signal.emit(create_msg("Viewing Cart before Checkout", "normal"))
-
-        while not at_checkout:
-            try:
-                checkout_btn = self.browser.find_element_by_xpath('//button[@data-test= "addToCartModalViewCartCheckout"]')
-                checkout_btn.click()
-                at_checkout = True
-            except:
-                continue
-
-    def checkout(self):
-        did_checkout = False
-        self.status_signal.emit(create_msg("Checking out", "normal"))
-
-        while not did_checkout:
-            try:
-                self.browser.find_element_by_xpath('//button[@data-test= "checkout-button"]').click()
-                did_checkout = True
-                time.sleep(random_delay(self.monitor_delay, settings.random_delay_start, settings.random_delay_stop))
-            except:
-                continue
-
-
-    def submit_billing(self):
-        added_cc = False
-        added_cvv = False
-
-        self.status_signal.emit(create_msg("Entering CC #", "normal"))
-
-        while not added_cc:
-            try:
-                cc_input = self.browser.find_element_by_id("creditCardInput-cardNumber")
-                cc_input.send_keys(self.profile["card_number"])
-                if len(self.browser.find_elements_by_xpath('//button[@data-test= "verify-card-button"]')) > 0:
-                    self.browser.find_element_by_xpath('//button[@data-test= "verify-card-button"]').click()
-                added_cc = True
-            except:
-                self.status_signal.emit(create_msg("CC Verification not needed", "normal"))
-                break
-
-        while not added_cvv:
-            try:
-                cvv_input = self.browser.find_element_by_id("creditCardInput-cvv")
-                self.status_signal.emit(create_msg("Entering CC Last 3", "normal"))
-                cvv_input.send_keys(self.profile["card_cvv"])
-
-                if len(self.browser.find_elements_by_xpath('//button[@data-test= "save-and-continue-button"]')) > 0:
-                    self.browser.find_element_by_xpath('//button[@data-test= "save-and-continue-button"]').click()
-                added_cvv = True
-            except:
-                self.status_signal.emit(create_msg("No need to enter last 3", "normal"))
-                break
+    def atc_and_checkout(self):
+        while not self.did_submit:
+            for xpath_step in self.xpath_sequence:
+                for attempt in range(self.retry_attempts + 1):
+                    try:
+                        wait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, xpath_step['path'])))
+                        self.process_step(xpath_step)
+                        break
+                    except:
+                        if attempt == self.retry_attempts:
+                            if not self.check_stock(new_tab=True):
+                                self.status_signal.emit(create_msg('Product is out of stock. Resuming monitoring.', 'error'))
+                                return
+                            else:
+                                self.status_signal.emit(create_msg('Encountered unknown page while product in stock. Quitting.', 'error'))
+                                self.failed = True
+                                return
+                        self.process_interruptions(attempt=attempt)
 
     def submit_order(self):
-        did_submit = False
-
-        self.status_signal.emit(create_msg("Submitting Order", "normal"))
+        self.did_submit = False
         url = self.browser.current_url
-        while not did_submit:
+        while not self.did_submit:
             try:
-                cvv_field = self.browser.find_elements_by_id('creditCardInput-cvv')
-                if cvv_field:
-                    if len(cvv_field[0].get_attribute('value')) == 0:
-                        cvv_field[0].send_keys(self.profile["card_cvv"])
-                self.browser.find_element_by_xpath('//button[@data-test="placeOrderButton"]').click()
-                time.sleep(5)
-                if url != self.browser.current_url:
+                self.process_interruptions(silent=True)
+                if not settings.dont_buy:
+                    self.browser.find_element_by_xpath('//button[@data-test="placeOrderButton"]').click()
+                    time.sleep(5)
+                if (url != self.browser.current_url and not self.browser.find_elements_by_id('creditCardInput-cvv')) or settings.dont_buy: #TODO: make this more robust
                     self.status_signal.emit(create_msg("Order Placed", "success"))
                     send_webhook("OP", "Target", self.profile["profile_name"], self.task_id, self.product_image)
-                    did_submit = True               
+                    self.did_submit = True
             except:
-                continue
+                self.status_signal.emit(create_msg('Retrying submit order until success', 'normal'))
+
+    def find_and_click(self, xpath):
+        self.browser.find_element_by_xpath(xpath).click()
+        
+    def find_and_click_atc(self):
+        if self.browser.current_url == self.product and self.check_stock():
+            if self.browser.find_elements_by_xpath('//button[@data-test= "orderPickupButton"]'):
+                button = self.browser.find_element_by_xpath('//button[@data-test= "orderPickupButton"]')
+            elif self.browser.find_elements_by_xpath('//button[@data-test= "shipItButton"]'):
+                button = self.browser.find_element_by_xpath('//button[@data-test= "shipItButton"]')
+            else:
+                button = None
+        if button:
+            self.browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+            self.atc_clicked = True
+            button.click()
+    
+    def fill_field_and_proceed(self, xpath, args):
+        input_field = self.browser.find_element_by_xpath(xpath)
+        if len(input_field.get_attribute('value')) == 0:
+            input_field.send_keys(args['value'])
+        if 'confirm_button' in args:
+            if self.browser.find_elements_by_xpath(args['confirm_button']):
+                self.find_and_click(args['confirm_button'])
+
+    def process_step(self, xpath_step, wait_after=False, silent=False):
+        if self.browser.find_elements_by_xpath(xpath_step['path']):
+            if not silent:
+                self.status_signal.emit(create_msg(xpath_step['message'], xpath_step['message_type']))
+            if xpath_step['type'] == 'button':
+                self.find_and_click(xpath_step['path'])
+            elif xpath_step['type'] == 'method':
+                xpath_step['method']()
+            elif xpath_step['type'] == 'input':
+                self.fill_field_and_proceed(xpath_step['path'], xpath_step['args'])
+            if wait_after:
+                time.sleep(self.TIMEOUT_SHORT)
+        
+    def process_interruptions(self, attempt=0, silent=False):
+        if not silent:
+            self.status_signal.emit(create_msg(f'Interrupted, attempting to resolve ({attempt+1}/{self.retry_attempts})', 'error'))
+        for xpath_step in self.possible_interruptions:
+            self.process_step(xpath_step, wait_after=True, silent=True)
