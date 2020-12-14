19ebb35889da6a2e92d5ee17178df3f59927f0aa
ima9rd <ima9rd@gmail.com>
2020-12-13 18:04:46 -0600

    Removing testing code

--- utils/restful.py
+++ utils/restful.py
@@ -9,16 +9,13 @@ class Item(BaseModel):
 
 @fastapi_app.post('/api/notification')
 async def trigger_event(item: Item):
-    try:
-        series = (item.link['series'])
-        store = (item.store['name'])
-        if store in stock_status:
-            stock_status[store][series] = True
-        else:
-            stock_status[store] = {series: True}
-    except:
-        print(item)
-        
+    series = (item.link['series'])
+    store = (item.store['name'])
+    if store in stock_status:
+        stock_status[store][series] = True
+    else:
+        stock_status[store] = {series: True}
+
 @fastapi_app.get('/api/notification')
 async def get_stock_status():
     return stock_status
